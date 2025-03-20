4c903903b6b2a79be2c49157ae73392ef7a3ac96
JackieOuzman <jackie.ouzman@csiro.au>
2025-03-20 14:33:36 +1030

    Step 1 complete

--- STEP1_decile_rainfall_cals2025.R
+++ STEP1_decile_rainfall_cals2025.R
@@ -23,7 +23,7 @@
 ## I have selected a APSIM format starting with 1/1/1960 and saved as a .csv file
 
 
-## Load Libraries -----------------------------------------------------------------
+## Load Libraries --------------------------------------------------------------
 
 library(ggplot2)
 library(readxl)
@@ -33,7 +33,7 @@ library(data.table)
 library(stringr)
 
 
-## Import met files -----------------------------------------------------------------
+## Set location of file --------------------------------------------------------
 
 #Folder for met files
 current.folder <- "H:/Output-2/Analysis/Scripts/Jackie/"
@@ -45,7 +45,8 @@ list.of.files #with path
 #list.of.files <- c(
 #  "H:/Output-2/Analysis/Scripts/Jackie/metfile_ Karoonda_025006.csv")
 
-### Download all of the climate data
+## Download all of the climate data and format it -------------------------------
+
 climate <- read.table(list.of.files, 
                       skip = 22, header = TRUE, sep ="")
 climate <- climate [-1,]
@@ -62,14 +63,35 @@ climate <- climate %>%
 
 ## convert the numeric clm from a char to double
 str(climate)
-climate <- climate %>% mutate_at(c("day_of_year" ,"radn","maxt", "mint", "rain", "evap", "vp"), as.numeric)
+climate <- climate %>% 
+  mutate_at(c("day_of_year" ,"radn","maxt", "mint", "rain", "evap", "vp"), as.numeric)
 
-#---
+## Get metadata and add to met file --------------------------------------------
 
-Cliamte_file_start_date <- min(climate$date)
-Cliamte_file_end_date <- max(climate$date)
 
-#alternative 
+##Add some meta data back into the file  
+## station number
+station_number <- read_csv(list.of.files,
+                           col_names = FALSE, skip = 1)
+station_number <-station_number[1,1] #just the row with the download date
+station_number <-stringr::str_extract(station_number, "[[:digit:]]+") #just the numbers
+
+## station name
+station_name <- read_csv(list.of.files, 
+                         col_names = FALSE, skip = 2)
+station_name <-station_name[1,1]
+station_name <- station_name %>% stringr::str_replace("!station name =", "")
+station_name<-str_trim(station_name) #remove the white spaces
+
+
+climate <- climate %>% 
+  mutate(station_number = station_number,
+         station_name = station_name)
+
+
+## Additional information about the years of data - supplementary data-------
+
+
 Cliamte_file_start_end_date <- climate %>% 
   summarise(Cliamte_file_start_date =  min(date),
             Cliamte_file_end_date =    max(date),
@@ -118,12 +140,6 @@ numb_days_year_before_end <- climate %>% filter(year==Year_before_end$Cliamte_fi
          numb_days = (last_day -first_day) +1) %>% 
   select(- last_day,  -first_day) 
 
-
-
-
-
-
-
 numb_days_start
 numb_days_end
 numb_days_year_before_end
@@ -133,30 +149,21 @@ numb_days_year_before_end
 details_about_start_end_climate_data <- rbind(numb_days_start, numb_days_end,numb_days_year_before_end )
 details_about_start_end_climate_data
 
+rm(numb_days_end, numb_days_start, numb_days_year_before_end, Year_before_end, Cliamte_file_start_end_date)
+
+## Save the df ready for Step 2 --------------------------------------------------------
+
+
+write_csv(climate, file = paste0(current.folder,"output_of_steps/","Step1.csv"))
+write_csv(details_about_start_end_climate_data, 
+          file = paste0(current.folder,"output_of_steps/","Step1_details_about_start_end_climate_data.csv"))
 
 
 ## Loop to import files -----------------------------------------------------------------
 
 
 #for (list.of.files in list.of.files){
-##Add some meta data back into the file  
-   ## station number
-  station_number <- read_csv(list.of.files,
-                             col_names = FALSE, skip = 1)
-  station_number <-station_number[1,1] #just the row with the download date
-  station_number <-stringr::str_extract(station_number, "[[:digit:]]+") #just the numbers
-  
-  ## station name
-  station_name <- read_csv(list.of.files, 
-                           col_names = FALSE, skip = 2)
-  station_name <-station_name[1,1]
-  station_name <- station_name %>% stringr::str_replace("!station name =", "")
-  station_name<-str_trim(station_name) #remove the white spaces
 
-  
-climate <- climate %>% 
-  mutate(station_number = station_number,
-         station_name = station_name)
  
 
 
