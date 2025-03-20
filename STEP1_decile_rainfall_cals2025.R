46233725d8efe5152edeab3060df637c9e7ce0aa
JackieOuzman <jackie.ouzman@csiro.au>
2025-03-20 13:15:22 +1030

    getting the think right and breaking the process into steps

--- STEP1_decile_rainfall_cals2025.R
+++ STEP1_decile_rainfall_cals2025.R
@@ -42,26 +42,105 @@ current.folder <- "H:/Output-2/Analysis/Scripts/Jackie/"
 list.of.files <- list.files(current.folder, ".csv",full.names=T) #the trick is getting the full name
 list.of.files #with path
 
-list.of.files <- c(
-  "H:/Output-2/Analysis/Scripts/Jackie/metfile_ Karoonda_025006.csv")
+#list.of.files <- c(
+#  "H:/Output-2/Analysis/Scripts/Jackie/metfile_ Karoonda_025006.csv")
+
+### Download all of the climate data
+climate <- read.table(list.of.files, 
+                      skip = 22, header = TRUE, sep ="")
+climate <- climate [-1,]
+climate <- climate %>% 
+  mutate(date = strptime(paste(year, day), format="%Y %j"))
+
+climate <- climate %>% 
+  rename(day_of_year = day) %>% 
+  mutate(year = year(date),
+         month =month(date),
+         day_of_month = lubridate::day(date),
+         month_name = lubridate::month(date, label = TRUE)
+         )
+
+## convert the numeric clm from a char to double
+str(climate)
+climate <- climate %>% mutate_at(c("day_of_year" ,"radn","maxt", "mint", "rain", "evap", "vp"), as.numeric)
+
+#---
+
+Cliamte_file_start_date <- min(climate$date)
+Cliamte_file_end_date <- max(climate$date)
+
+#alternative 
+Cliamte_file_start_end_date <- climate %>% 
+  summarise(Cliamte_file_start_date =  min(date),
+            Cliamte_file_end_date =    max(date),
+              ) %>% 
+  mutate(Cliamte_file_start_Year =       year(Cliamte_file_start_date),#get the year
+         Cliamte_file_end_Year =         year(Cliamte_file_end_date)
+         ) 
+
+Year_before_end <- Cliamte_file_start_end_date %>% 
+  mutate(Cliamte_file_date_before_end = ymd(Cliamte_file_end_date)-years(1) ,
+         Cliamte_file_Year_before_end = year(Cliamte_file_date_before_end))%>% 
+  select(Cliamte_file_date_before_end,Cliamte_file_Year_before_end )
+
+str(Year_before_end)
+str(Cliamte_file_start_end_date)
+
+numb_days_start <- climate %>% filter(year==Cliamte_file_start_end_date$Cliamte_file_start_Year) %>% 
+  summarise(first_day = min(day_of_year),
+            last_day = max(day_of_year)
+            ) %>% 
+  mutate(Cliamte_file_position = "Cliamte_file_start",
+         Cliamte_file_date  = Cliamte_file_start_end_date$Cliamte_file_start_date,
+         Cliamte_file_year  = Cliamte_file_start_end_date$Cliamte_file_start_Year,
+         numb_days = (last_day -first_day) +1) %>% 
+           select(- last_day,  -first_day)
+
+numb_days_end <- climate %>% filter(year==Cliamte_file_start_end_date$Cliamte_file_end_Year) %>% 
+  summarise(first_day = min(day_of_year),
+            last_day = max(day_of_year)
+  ) %>% 
+  mutate(Cliamte_file_position = "Cliamte_file_end",
+         Cliamte_file_date  = Cliamte_file_start_end_date$Cliamte_file_end_date,
+         Cliamte_file_year  = Cliamte_file_start_end_date$Cliamte_file_end_Year,
+         numb_days = (last_day -first_day) +1) %>% 
+  select(- last_day,  -first_day)   
+
+str(Year_before_end)
+
+numb_days_year_before_end <- climate %>% filter(year==Year_before_end$Cliamte_file_Year_before_end) %>% 
+  summarise(first_day = min(day_of_year),
+            last_day = max(day_of_year)
+  ) %>% 
+  mutate(Cliamte_file_position = "Cliamte_file_year_before_end",
+         Cliamte_file_date  = Year_before_end$Cliamte_file_date_before_end,
+         Cliamte_file_year  = Year_before_end$Cliamte_file_Year_before_end,
+         numb_days = (last_day -first_day) +1) %>% 
+  select(- last_day,  -first_day) 
 
 
 
-download_date_for_all_sites <-"20250319"  #YYYYMMDD
-start_date <- "2025/1/1" #"1920/1/1"
+
+
+
+
+numb_days_start
+numb_days_end
+numb_days_year_before_end
+
+## merge them ###
+
+details_about_start_end_climate_data <- rbind(numb_days_start, numb_days_end,numb_days_year_before_end )
+details_about_start_end_climate_data
+
+
 
 ## Loop to import files -----------------------------------------------------------------
 
 
 #for (list.of.files in list.of.files){
-  
-  download_date <- as.Date(as.character(download_date_for_all_sites),format="%Y%m%d")
-  
-  #minus one day from download
-  download_date <- lubridate::ymd(download_date) - days(1)
-  
-  
-  ## station number
+##Add some meta data back into the file  
+   ## station number
   station_number <- read_csv(list.of.files,
                              col_names = FALSE, skip = 1)
   station_number <-station_number[1,1] #just the row with the download date
@@ -73,20 +152,17 @@ start_date <- "2025/1/1" #"1920/1/1"
   station_name <-station_name[1,1]
   station_name <- station_name %>% stringr::str_replace("!station name =", "")
   station_name<-str_trim(station_name) #remove the white spaces
+
   
-  ### Download all of the cliamte data
-  Cliamte <- read.table(list.of.files, 
-                        skip = 22, header = TRUE, sep ="")
-  Cliamte <- Cliamte [-1,]
-  
-  ### need to make a clm that has all the dates not just day of year and year
-  str(Cliamte)
-  Cliamte$rain <- as.character(Cliamte$rain)
-  Cliamte$rain <- as.double(Cliamte$rain)
-  
-  #create a date clm # note you need to be aweare of the last day of downlaoded met data
-  
-  #date range
+climate <- climate %>% 
+  mutate(station_number = station_number,
+         station_name = station_name)
+ 
+
+
+
+### ---- stop step 1 here and save outputs
+ 
   
   ## EXTRA STEP Trim dates to fixed period
   
