a9c2015cbe0800ce2ef1772875b187ff36ad48f3
JackieOuzman <jackie.ouzman@csiro.au>
2025-04-01 14:00:46 +1030

    tidy up

--- Temp/STEP2_decile_rainfall_cals2025.R
+++ Temp/STEP2_decile_rainfall_cals2025.R
@@ -14,72 +14,71 @@ library(stringr)
 ## Set location of file --------------------------------------------------------
 
 #Folder for met files
-location_files <- "H:/Output-2/Analysis/Scripts/Jackie/Karoonda_025006/"
-
+location_files <- "H:/Output-2/Analysis/Scripts/Jackie/Downloaded_files/"
+saved_files <- "H:/Output-2/Analysis/Scripts/Jackie/Decile_tables/"
+site_number <- 25006
 # find the files that you want
 list.of.files <- list.files(location_files, ".csv",full.names=T) #the trick is getting the full name
 list.of.files #with path
 
 
 ## Download all of the climate data and format it -------------------------------
-Step1 <- read_csv("H:/Output-2/Analysis/Scripts/Jackie/Karoonda_025006/Step1.csv")
-
-Step1_details_about_start_end_climate_data <- read_csv("H:/Output-2/Analysis/Scripts/Jackie/Karoonda_025006/Step1_details_about_start_end_climate_data.csv")
-
+Step1 <- read_csv(
+  paste0(location_files, "Neater_file_", site_number, ".csv"))
+  
 #### set / check the date clms as dates
 Step1
-str(Step1$date)
-str(Step1_details_about_start_end_climate_data$Cliamte_file_date)
+str(Step1$Date)
 
 # 1.What years do you want to calculate the deciles for? ------------------------
-Step1_details_about_start_end_climate_data
-start_year <- "1920-01-01" # 1920 #
-end_year <- "2024-12-31" # 2024 #
+
+start_year <- year(min(Step1$Date))
+end_year <- year(max(Step1$Date)) #you can change this to last year 2024
 
 # 2. How to defined the GS rainfall  ---------------------------------------------
 
-DAY_start_GS_rainfall <- 1
+Day_start_GS_rainfall <- 1
 Month_start_GS_rainfall <- 4 #April
 
-DAY_end_GS_rainfall <- 15
+Day_end_GS_rainfall <- 15
 Month_end_GS_rainfall <- 10 #Oct
 
-GS_Defined <- paste0(DAY_start_GS_rainfall,"/", Month_start_GS_rainfall, " to ", 
-                     DAY_end_GS_rainfall,"/", Month_end_GS_rainfall)
+GS_Defined <- paste0(Day_start_GS_rainfall,"/", Month_start_GS_rainfall, " to ", 
+                     Day_end_GS_rainfall,"/", Month_end_GS_rainfall)
 
 ### Filter dataset based on years  ---------------------------------------------
-Step1_set_date <- Step1 %>% filter(between(date, as.Date(start_year), as.Date(end_year)))
-str(Step1_set_date)
+# Step1_set_date <- Step1 %>% filter(between(date, as.Date(start_year), as.Date(end_year)))
+# str(Step1_set_date)
 
 
 # 3.Create season clm per day as GS or summer ---------------------------------------------
 
-Step1_set_date <- Step1_set_date %>% 
+Step1 <- Step1 %>% 
   mutate(
     start_end_GS_date = case_when(
       month == Month_start_GS_rainfall & 
-        day_of_month  == DAY_start_GS_rainfall ~ "start_gs" ,
+        day_of_month  == Day_start_GS_rainfall ~ "start_gs" ,
       month == Month_end_GS_rainfall & 
-        day_of_month  == DAY_end_GS_rainfall+1 ~ "end_gs" ,
+        day_of_month  == Day_end_GS_rainfall+1 ~ "end_gs" ,
       TRUE ~ NA
     ))
-Step1_set_date <- Step1_set_date %>% fill(start_end_GS_date) %>% 
+Step1 <- Step1 %>% fill(start_end_GS_date) %>% 
   mutate(
     season = case_when(
       start_end_GS_date == "start_gs" ~ "gs",
       TRUE ~ "summer"))
 
-Step1_set_date <- Step1_set_date %>% select(-start_end_GS_date)
+Step1 <- Step1 %>% select(-start_end_GS_date)
 
 
 # 4.Calculate the sum of rain for each season in the year  ------------------------
 
-Summary_rain_season_yr <- Step1_set_date %>% 
+Summary_rain_season_yr <- Step1 %>% 
   group_by(year, season) %>% 
-  summarise(sum_rain_season = sum(rain, na.rm = TRUE))
+  summarise(sum_rain_season = sum(Rain, na.rm = TRUE))
 Summary_rain_season_yr <- ungroup(Summary_rain_season_yr)
 str(Summary_rain_season_yr)
-
+str(Step1)
 
 
 
@@ -90,9 +89,9 @@ str(Summary_rain_season_yr)
 plot1 <- ggplot(data = Summary_rain_season_yr,aes(x = year, y = sum_rain_season, fill = season) )+
        geom_bar(stat = "identity")+
   labs(title = "Sum of rain per year",
-       subtitle = paste0("Station name: ", distinct(Step1, station_name), 
-                         " - ", distinct(Step1, station_number)),
-       caption = paste0("Years included : ",year(start_year) ," to ",  year(end_year),
+       subtitle = paste0("Station name: ", distinct(Step1, Name), 
+                         " - ", distinct(Step1, Number)),
+       caption = paste0("Years included : ",start_year ," to ", end_year,
                         ". GS defined as: ", GS_Defined),
        y = "sum of rain"
     )
@@ -121,8 +120,8 @@ GS_decile_table <- GS_decile_table %>%  select (GS_deciles_names, GS_decile_min_
 
 
 GS_decile_table <- GS_decile_table %>% 
-  mutate(Station_name = paste0(distinct(Step1, station_name)),
-         Station_number = paste0(distinct(Step1, station_number)) ,
+  mutate(Station_name = paste0(distinct(Step1, Name)),
+         Station_number = paste0(distinct(Step1, Number)) ,
          Years_included = paste0(year(start_year) ," to ",  year(end_year)),
          GS_period =   GS_Defined
          )
@@ -155,9 +154,9 @@ gs_rain <- gs_rain %>%
 
 
 gs_rain <- gs_rain %>% 
-  mutate(Station_name = paste0(distinct(Step1, station_name)),
-         Station_number = paste0(distinct(Step1, station_number)) ,
-         Years_included = paste0(year(start_year) ," to ",  year(end_year)),
+  mutate(Station_name = paste0(distinct(Step1, Name)),
+         Station_number = paste0(distinct(Step1, Number)) ,
+         Years_included = paste0(start_year ," to ",  end_year),
          GS_period =   GS_Defined                      
   )
 
@@ -170,7 +169,8 @@ gs_rain
 GS_decile_table
 
 write_csv(GS_decile_table, 
-          file = paste0("H:/Output-2/Analysis/Scripts/Jackie/Karoonda_025006/","GS_decile_table.csv"))
+          file =paste0(saved_files, "GS_decile_table_", site_number, ".csv"))
+          
 
 ## 5b. Summer ----------------------------------------------------------------------
 #the percentiles for each year.
@@ -192,9 +192,9 @@ Summer_decile_table[1,3] <-  min(summer_rain$sum_rain_season)
 Summer_decile_table <- Summer_decile_table %>%  select (Summer_deciles_names, Summer_decile_min_rain, Summer_decile_max_rain)
 
 Summer_decile_table <- Summer_decile_table %>% 
-  mutate(Station_name = paste0(distinct(Step1, station_name)),
-         Station_number = paste0(distinct(Step1, station_number)) ,
-         Years_included = paste0(year(start_year) ," to ",  year(end_year)),
+  mutate(Station_name = paste0(distinct(Step1, Name)),
+         Station_number = paste0(distinct(Step1, Number)) ,
+         Years_included = paste0(start_year ," to ",  end_year),
          GS_period =   GS_Defined
   )
 
@@ -232,8 +232,12 @@ summer_rain <- summer_rain %>%  rename(Summer_sum_rain = sum_rain_season,
 
 ## Save / export outputs  ------------------------------------------------------
 Summer_decile_table
+
 write_csv(Summer_decile_table, 
-          file = paste0("H:/Output-2/Analysis/Scripts/Jackie/Karoonda_025006/","summer_decile_table.csv"))
+          file =paste0(saved_files, "Summer_decile_table_", site_number, ".csv"))
+
+
+
 
 summer_rain
 gs_rain
@@ -254,15 +258,18 @@ Year_with_Deciles
 rm(gs_rain, summer_rain)
 Year_with_Deciles
 
-########STOP#######################################################################
+
 
 ##files
 
+
 write_csv(Year_with_Deciles, 
-          file = paste0("H:/Output-2/Analysis/Scripts/Jackie/Karoonda_025006/","Year_with_Deciles.csv"))
+          file =paste0(saved_files, "Year_with_Deciles_", site_number, ".csv"))
+
+
 
 ##plots
-ggsave(plot = plot1,
-       filename =  paste0("H:/Output-2/Analysis/Scripts/Jackie/Karoonda_025006/","plot1_sum_rain_per_yr_summer_GS.png"), 
-       width = 20, height = 12, units = "cm")
+# ggsave(plot = plot1,
+#        filename =  xxxx","plot1_sum_rain_per_yr_summer_GS.png"), 
+#        width = 20, height = 12, units = "cm")
 
