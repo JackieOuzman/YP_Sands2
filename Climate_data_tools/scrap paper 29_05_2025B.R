6f2fef6802051c1044fc45b579c8e46e2c31c53c
JackieOuzman <jackie.ouzman@csiro.au>
2025-05-30 16:15:02 +0930

    looking good and it working

--- Climate_data_tools/scrap paper 29_05_2025B.R
+++ Climate_data_tools/scrap paper 29_05_2025B.R
@@ -92,6 +92,100 @@ df_gs_base <- df_gs %>% select(Defined_date_common_year, GS_deciles_names, GS_ra
                                
 
 
+# * **Frontier**
+# * 1. ` Estimated Starting Water (mm) = Summer rainfall *.30
+# * 2. `Estimated Evaporation` = (Estimated Rainfall to maturity + GSR to date) * 0.18 +15
+# * 3. `Estimated Water Supply` = (GS Rain + Estimated rainfall to maturity+ Estimated Starting water) - Estimated Evaporation
+# * 4. `Estimated Yield Potential (kg/ha) = Estimated Water Supply *26
+# * 5. Yield_forecast = 80% * `EPY` 
 
 
+df_gs <- df_gs %>% 
+  mutate(est_sw_front = (summer_rain_current_yr*.30),
+         
+         est_evap_front = (GS_rainfall_defined_date_historical +GS_rainfall_defined_date_current_yr)*0.18+15,
+         
+         est_water_supp_front = ((GS_rainfall_defined_date_current_yr + 
+                                   GS_rainfall_defined_date_historical+ 
+                                   est_sw_base) -   est_evap_base),
+         
+         est_yp_front = (est_water_supp_front *26),
+         Yld_forecast_front = (est_yp_front*.80)
+  )
+
+
+names(df_gs)
+
+
+
+df_gs_front <- df_gs %>% select(Defined_date_common_year, GS_deciles_names, GS_rainfall_defined_date_historical,
+                               Station_name, Station_number, summer_rain_current_yr, GS_rainfall_defined_date_current_yr,
+                               
+                               est_sw_front,
+                               est_evap_front,
+                               est_water_supp_front,
+                               est_yp_front,
+                               Yld_forecast_front)
+
+
+### Reformat for plotting
+names(df_gs)
+df_plot <- df_gs %>%
+  select(
+    GS_deciles_names,
+    Defined_date_common_year,
+    Station_name,
+    Station_number,
+    Yld_forecast_base,
+    Yld_forecast_front,
+    est_yp_front
+  ) %>%
+  rename(Yld_forecast_future = est_yp_front)
+
+df_plot_long <- df_plot %>% pivot_longer(
+  cols = starts_with("Yld"),
+  names_to = "Method",
+  values_to = "Yield")
+  
+  
+df_plot_long
+
+
+
+df_plot_long$GS_deciles_names <- factor(df_plot_long$GS_deciles_names , ordered = TRUE, 
+                                           levels = c("decile_1", "decile_2", "decile_3", "decile_4",
+                                                      "decile_5", "decile_6", "decile_7", "decile_8",
+                                                      "decile_9","decile_10" ))
+df_plot_long %>% distinct(Defined_date_common_year)
+
+df_plot_long$defined_date <- factor(
+  df_plot_long$Defined_date_common_year ,
+  ordered = TRUE,
+  levels = c(
+    "2023-04-01",
+    "2023-05-01",
+    "2023-06-01",
+    "2023-07-01",
+    "2023-08-01",
+    "2023-09-01",
+    "2023-10-01"
+  )
+)
+
+
+plot1 <- df_plot_long %>% 
+  filter(GS_deciles_names  == "decile_3" |GS_deciles_names  == "decile_5"| GS_deciles_names  == "decile_7" ) %>% 
+  ggplot(aes(x = Defined_date_common_year , y = Yield , group=Method) )+
+  geom_point(aes(color=Method))+
+  geom_line(aes(color=Method))+
+  theme_bw()+
+  facet_wrap(.~GS_deciles_names)+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  labs(title = "Forecasted yield potential from defined date in the GS using different methods, by decile years",
+       subtitle = paste0("Year: ", year,  ". Site: ", site_selected),
+       y = "Forecasted yield potential",
+       x = "Dates in the growing season",
+       colour = "" #removed the legend title
+  )
+plot1
 
